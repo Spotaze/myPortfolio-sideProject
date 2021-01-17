557c3b0dd4a1e2323c82e3bc5d993de9df5a41cb
EdgaDEV <cyanklegaming@gmail.com>
2021-01-17 23:44:58 +0200

    second commit

--- app.js
+++ app.js
@@ -9,7 +9,32 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
+//Text animation
+const text = document.querySelector('.hero_heading');
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent= "";
+for(let i = 0; i < splitText.length; i++){
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+let char = 0;
+let timer = setInterval(onTick, 50);
 
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if(char === splitText.length){
+        complete();
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
+
+//Highlighting active page on navigation bar
 const highlightMenu = () => {
     const elem = document.querySelector('.highlight');
     const homeMenu = document.querySelector('#home-page');
